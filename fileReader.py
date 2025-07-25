12dcf264456bae26ab831eb1f9d858f7c42a6ec0
J-Alfred-Prufrock <angusrt64@gmail.com>
2025-07-25 15:42:42 +1000

    currently in the works of adding the file reader

--- fileReader.py
+++ fileReader.py
@@ -1,13 +1,22 @@
+from node import node
+
 class fileReader():
     def __init__(self, fileName):
         self.fileName = fileName
         f = open(fileName+".txt")
+
         mazeWidth = int(f.readline().strip())
         mazeHeight = int(f.readline().strip())
 
+        tempTestArray = []
+        xCounter = 0
+        yCounter = 0
+
         print(mazeWidth,mazeHeight)
         for lines in f:
-             print(lines[1])
+             for i in lines:
+                 n = node(xCounter,yCounter,lines[i],mazeWidth,mazeHeight)
+                 tempTestArray.insert(node)
         
         f.close()
 
